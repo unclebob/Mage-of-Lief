038ae0a9ddc9bfe8ded27a5c39561eb602b41b31
unclebob <unclebob@objectmentor.com>
2009-08-13 15:30:53 -0500

    Born

--- spec/life_spec.rb
+++ spec/life_spec.rb
@@ -3,7 +3,7 @@ require 'spec'
 describe "Life Rules" do
   context "Already Living" do
     ALL_NEIGHBORS = (0..8).to_a
-    STAYS_LIVING_NEIGHBOR_COUNTS = [2,3]
+    STAYS_LIVING_NEIGHBOR_COUNTS = [2, 3]
     STOPS_LIVING_NEIGHBOR_COUNTS = ALL_NEIGHBORS - STAYS_LIVING_NEIGHBOR_COUNTS
 
     def stays_alive?(neighbor_count)
@@ -22,6 +22,19 @@ describe "Life Rules" do
 
   end
   context "Already Dead" do
-
+    STARTS_LIVING_NEIGHBOR_COUNTS = [3]
+    STAYS_DEAD_NEIGHBOR_COUNTS = ALL_NEIGHBORS-STARTS_LIVING_NEIGHBOR_COUNTS
+    def stays_dead?(neighbor_count)
+      STAYS_DEAD_NEIGHBOR_COUNTS.include? neighbor_count
+    end
+    {STAYS_DEAD_NEIGHBOR_COUNTS => true, STARTS_LIVING_NEIGHBOR_COUNTS => false}.each_pair do |neighbor_counts, stays_dead|
+      neighbor_counts.each do |neighbor_count|
+        context "#{neighbor_count} neighbors" do
+          it "#{stays_dead ? 'stays dead' : 'is born'}" do
+            stays_dead?(neighbor_count).should == stays_dead
+          end
+        end
+      end
+    end 
   end
 end
